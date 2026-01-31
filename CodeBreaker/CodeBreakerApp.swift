01b54c0ff64d7b213f8dd1f400de7a56525869d8
test <petr.ahtimirov@gmail.com>
2026-01-31 10:33:24 +0300

    init

---
+++ CodeBreaker/CodeBreakerApp.swift
@@ -0,0 +1,17 @@
+//
+//  CodeBreakerApp.swift
+//  CodeBreaker
+//
+//  Created by aht21 on 31.01.2026.
+//
+
+import SwiftUI
+
+@main
+struct CodeBreakerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ CodeBreaker/ContentView.swift
@@ -0,0 +1,23 @@
+//
+//  ContentView.swift
+//  CodeBreaker
+//
+//  Created by aht21 on 31.01.2026.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+            Text("greetings!")
+            Text("howdy!")
+            Circle()
+        }
+    }
+}
+
+#Preview {
+    ContentView()
+}
